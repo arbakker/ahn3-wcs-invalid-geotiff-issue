f2b7baff551c61fea2b97ee55d9705d044aa7bd9
Anton Bakker <a.r.bakker1@gmail.com>
2022-08-31 14:43:59 +0200

    wip

--- do-getcov-req.sh
+++ do-getcov-req.sh
@@ -2,17 +2,18 @@
 set -euo pipefail
 
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-SERVICE_URL="${1:-https://service.pdok.nl/rws/ahn3/wcs/v1_0}"
-COVERAGE_ID="${2:-ahn3_5m_dtm}"
+OUTPUT_DIR="${1}"
+RUN_TYPE="${2}"
+SERVICE_URL="${3:-https://service.pdok.nl/rws/ahn3/wcs/v1_0}"
+COVERAGE_ID="${4:-ahn3_5m_dtm}"
 
-bbox=231520,580797,235720,584997 # 100 bigger
-    
+bbox=231620,580897,235620,584897
+
 IFS=, read -r minx miny maxx maxy <<< "$bbox"
 scale_x=1000
 scale_y=1000
 
 query_v2="version=2.0.1&request=GetCoverage&service=WCS&CoverageID=${COVERAGE_ID}&crs=http://www.opengis.net/def/crs/EPSG/0/28992&format=image/tiff&scalesize=x($scale_x),y($scale_y)&subset=x($minx,$maxx)&subset=y($miny,$maxy)"
-
 query_v1="SERVICE=WCS&VERSION=1.0.0&REQUEST=GetCoverage&FORMAT=GEOTIFF_FLOAT32&COVERAGE=${COVERAGE_ID}&BBOX=${bbox}&CRS=EPSG:28992&RESPONSE_CRS=EPSG:28992&WIDTH=${scale_x}&HEIGHT=${scale_y}"
 
 # v1
@@ -23,9 +24,10 @@ output_tif_file=$(mktemp --suffix=.tif)
 output_cog_file=$(mktemp --suffix=.cog.tif)
 echo "> downloading tif from ${local_url} to ${output_tif_file}"
 curl -s $local_url -o $output_tif_file
+cp "$output_tif_file" "$OUTPUT_DIR/${RUN_TYPE}_output_100.tif"
 gdal_translate -r bilinear -of COG -co COMPRESS=DEFLATE $output_tif_file $output_cog_file > /dev/null
 echo "> v1 wcs request - succesfully converted tif to cog with gdal_translate: ${output_cog_file}"
-echo 
+
 
 # v2
 echo "#### WCS V2 ####"
@@ -35,5 +37,6 @@ output_tif_file=$(mktemp --suffix=.tif)
 output_cog_file=$(mktemp --suffix=.cog.tif)
 echo "> downloading tif from ${local_url} to ${output_tif_file}"
 curl -s $local_url | "${SCRIPT_DIR}/split-multipart.sh" - $output_tif_file
+cp "$output_tif_file" "$OUTPUT_DIR/${RUN_TYPE}_output_201.tif"
 gdal_translate -r bilinear -of COG -co COMPRESS=DEFLATE $output_tif_file $output_cog_file > /dev/null
 echo "> v2 wcs request - succesfully converted tif to cog with gdal_translate: ${output_cog_file}"
--- run-docker.sh
+++ run-docker.sh
@@ -21,7 +21,7 @@ if [[ $IMAGE == "PDOK" ]];then
         --name $CONTAINER_NAME \
         -v `pwd`:/srv \
         -v `pwd`/service.map:/etc/service.map \
-        pdok/mapserver:$VERSION > /dev/null
+        docker.io/pdok/mapserver:7.6.4-patch5-buster-lighttpd-nl > /dev/null
 else
     # run with camptocamp/mapserver
     VERSION=7.6
@@ -42,42 +42,5 @@ fi
 
 sleep 2
 
-QUERY_STRING_201="&version=2.0.1&request=GetCoverage&service=WCS&CoverageID=ahn3_5m_dtm&crs=http://www.opengis.net/def/crs/EPSG/0/28992&format=image/tiff&scalesize=x(1000),y(1000)&subset=x(231620,235620)&subset=y(580897,584897)"
-
-
-QUERY_STRING_100="SERVICE=WCS&VERSION=1.0.0&REQUEST=GetCoverage&FORMAT=GEOTIFF_FLOAT32&COVERAGE=ahn3_5m_dtm&BBOX=231620,580897,235620,584897&CRS=EPSG:28992&RESPONSE_CRS=EPSG:28992&WIDTH=1000&HEIGHT=1000&INTERPOLATION=bilinear"
-
-# do requests using mapserv binary
-
-output_cog_file_201=$(mktemp --suffix=.cog.tif)
-output_tif_file_201=$(mktemp --suffix=.tif)
-output_file_201=$(mktemp)
-
-output_cog_file_100=$(mktemp --suffix=.cog.tif)
-output_tif_file_100=$(mktemp --suffix=.tif)
-
-echo "mapserv -nh QUERY_STRING=\"${QUERY_STRING_201}\" > /tmp/output_wcs_getcov_201"
-docker exec -e QUERY_STRING="$QUERY_STRING_201" -it $CONTAINER_NAME bash -c "mapserv -nh QUERY_STRING=\"${QUERY_STRING_201}\" > /tmp/output_wcs_getcov_201"
-docker exec -e QUERY_STRING="$QUERY_STRING_100" -it $CONTAINER_NAME bash -c "mapserv -nh QUERY_STRING=\"${QUERY_STRING_100}\" > /tmp/output_wcs_getcov_100.tif"
-docker cp $CONTAINER_NAME:/tmp/output_wcs_getcov_201 "$output_file_201"
-docker cp $CONTAINER_NAME:/tmp/output_wcs_getcov_100.tif "$output_tif_file_100"
-
-if [[ $IMAGE == "PDOK" ]];then
-    "${SCRIPT_DIR}/split-multipart.sh" "$output_file_201" "$output_tif_file_201"
-else
-    mv "$output_file_201" "$output_tif_file_201"
-fi
-
-gdal_translate -r bilinear -of COG -co COMPRESS=DEFLATE "$output_tif_file_100" "$output_cog_file_100" > /dev/null
-echo "> v1 wcs request - succesfully converted tif to cog with gdal_translate: ${output_cog_file_100}"
-
-
-gdal_translate -r bilinear -of COG -co COMPRESS=DEFLATE "$output_tif_file_201" "$output_cog_file_201" > /dev/null
-echo "> v2 wcs request - succesfully converted tif to cog with gdal_translate: ${output_cog_file_201}"
-echo ""
-echo "diff <(gdalinfo -stats ${output_cog_file_100}) <(gdalinfo -stats ${output_cog_file_201})"
-# show logs
-# docker logs $CONTAINER_NAME
-
-# stop and remove container
-# docker stop $CONTAINER_NAME
+$SCRIPT_DIR/do-getcov-req.sh "./data" "docker" http://localhost
+exit 0
